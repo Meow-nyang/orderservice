d279b8dcf67cf3418b27dd02e40a3ff6d4910329
Meow_nyang <slah76@naver.com>
2025-04-22 10:33:29 +0900

    Product Entity 생성, BaseTime, Address 처리

--- src/main/java/com/playdata/orderservice/user/entity/User.java
+++ src/main/java/com/playdata/orderservice/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.playdata.orderservice.user.entity;
 
+import com.playdata.orderservice.common.entity.Address;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -24,9 +25,12 @@ public class User {
     @Column(unique = true, nullable = false)
     private String email;
 
+    @Embedded // @Embeddable로 선언된 값 대입 (기본 생성자 필수)
+    private Address address;
+
     @Enumerated(EnumType.STRING)
-    @Builder.Default // builder 패턴 사용해서 객체 초기화 시 전달한 값으로 세팅해 주라.
-    private Role role;
+    @Builder.Default // builder 패턴 사용해서 객체 초기화 시 초기값으로 세팅
+    private Role role = Role.USER;
 
 
 }
